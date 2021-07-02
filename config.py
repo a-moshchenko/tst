58e1d9c28d49025525a4cceda8c801b7d109b2de
artem.ig19909 <artem.ig1990@gmail.com>
2021-07-02 12:08:56 +0300

    создание нового пользователя по логин-паролю

---
+++ config.py
@@ -0,0 +1,8 @@
+SITE = "https://dev.nuxbet.com/"  # url сайта, на котором будем проводить тест
+EXECUTABLE_PATH = r"C:\chromedriver\chromedriver"  # Тут указать путь к файлу драйвера браузера
+PASSWD = "secretZ1"
+REFCODE = "QwERty123!@#"
+AUTHNAME = "autotestuser1672@mail.com"
+PASSWORD = "secretZ1"
+AUTH_NAME_EXIST = "autotestuser1672@mail.com" # мейл существующего пользователя
+DEFAULTMAIL = "nuxbetchk@gmail.com"
