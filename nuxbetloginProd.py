7778e8a43de1052d176fc3c9c728c2d430fb319a
a_moshchenko <alexssssf@gmail.com>
2021-07-27 16:08:02 +0300

    [REFACTORING] update tests.py and nuxbetloginProd.py

--- nuxbetloginProd.py
+++ nuxbetloginProd.py
@@ -1,134 +1,72 @@
-from time import sleep
-import commonFunctions
+from enum import Enum
+import time
 import config
 
-browser = commonFunctions.browser
-screenshot_path = config.SCREENSHOT_PATH_AUTHORISATION
-main_page_checkpoint = "/html/body/div/div[2]/div/section[2]/div"
-logout_dropdown_menu = "/html/body/div/div[2]/div/section[4]/header"
+
+class Locators(str, Enum):
+
+    GO_TO_LOGIN = "//a[@class='loginBtn']"
+    LOGIN_FORM = "//div[@class='formHeader flexCenter']/h2"
+    INPUT_FIELD = "//input[@type='text']"
+    PASSWORD_FIELD = "//input[@type='password']"
+    LOGIN_BUTTON = "//button[@class='mainBtn']"
+    VIEW_PASSWORD_BUTTON = "//div[@class='showPass curPointer']"
+    USERNAME = "//span[@class='userName ellipsis']"
+    ERROR_MESSAGE = "//div[@class='error danger-color']"
+    INCORRECT_INPUT = "//div[@class='error']"
+    DROP_DOWN = "//div[@class='userWrap curPointer logined allBtn']"
+    LOGOUT = "//a[@href='#']"
 
 
-def password_visibility_check():
-    # Проверяем отображение пароля при нажатии на глаз
-    browser.find_element_by_css_selector(".showPass").click()
-    sleep(1)
-    password_vizible = str(browser.find_element_by_xpath("(//input[@type='text'])[2]").get_attribute("value"))
-    try:
-        if password_vizible == config.PASSWORD:
-            browser.save_screenshot(f"{config.SCREENSHOT_PATH_AUTHORISATION}VisiblePasswordNuxbet.png")
-            print("password vizible, OK")
-        else:
-            print("password vizible, NotOK")
-    except Exception as e:
-        print(f"password vizible, Error, {e}\n password: {password_vizible}")
+class LoginTest:
 
+    def __init__(self, driver):
+        self.driver = driver
+        self.base_url = config.SITE_PROD
 
-def login_positive_flow():
-    commonFunctions.open_page(config.SITE_PROD, main_page_checkpoint)
-    browser.find_element_by_css_selector(".loginBtn").click()
+    def open_site(self):
+        self.driver.go_to(self.base_url)
 
-    try:
-        browser.find_element_by_css_selector(".formHeader")
-        print("auth form, OK")
-    except Exception as e:
-        print(f"auth form, NotOK, {e}")
-    login_mail = browser.find_element_by_xpath("//input[@type='text']")
-    login_mail.send_keys(config.AUTHORISATION_NAME_EXIST)
-    password = browser.find_element_by_xpath("//input[@type='password']")
-    password.send_keys(config.PASSWORD)
-    sleep(1)  # слип нужен, чтоб изменения отобразились в браузере
-    password_visibility_check()
-    login_button = browser.find_element_by_css_selector(".btnWrap > .mainBtn")
-    login_button.click()
-    sleep(2)
-    try:
-        uname = browser.find_element_by_xpath("//span[@class='userName ellipsis']")
-        if uname.text == config.AUTHORISATION_NAME_EXIST:
-            browser.save_screenshot(f"{config.SCREENSHOT_PATH_AUTHORISATION}LogedInNuxbet.png")
-            print("auth, OK\nmain page return, OK")
-        else:
-            print("NOK, uname: ", uname.text)
-    except Exception as e:
-        print(f"auth, NotOK, {e}")
-    log_out()
+    def open_login_form(self):
+        self.driver.interaction_with(Locators.GO_TO_LOGIN, clickable=True, click=True)
+        time.sleep(.3)
+        return self.driver.interaction_with(Locators.LOGIN_FORM).text
 
+    def valid_login(self):
+        self.driver.interaction_with(Locators.INPUT_FIELD, text='some_text')
+        self.log_in()
+        return self.driver.interaction_with(Locators.ERROR_MESSAGE)
 
-def log_out():
-    commonFunctions.wait_for_element(logout_dropdown_menu)
-    print("page loaded")
-    try:
-        browser.find_element_by_xpath("//span[@class='userName ellipsis']").click()
-        sleep(2)
-        browser.find_element_by_xpath("//a[@href='#']").click()
-    except Exception as e:
-        print(f"logged out, {e}")
+    def incorrect_login(self):
+        self.driver.interaction_with(Locators.INPUT_FIELD, text=config.AUTHORISATION_NAME[1:])
+        self.input_password()
+        self.log_in()
+        return self.driver.interaction_with(Locators.INCORRECT_INPUT).text
 
+    def incorrect_password(self):
+        self.input_login()
+        self.driver.interaction_with(Locators.PASSWORD_FIELD, text=config.AUTHORISATION_NAME[1:])
+        self.log_in()
+        return self.driver.interaction_with(Locators.INCORRECT_INPUT).text
 
-def login_negative_flow():
-    commonFunctions.open_page(config.SITE_PROD, main_page_checkpoint)
-    try:
-        log_out()
-    except Exception as e:
-        print(f"Loged out, {e}")
-    browser.find_element_by_css_selector(".loginBtn").click()
-    try:
-        browser.find_element_by_css_selector(".formHeader")
-        print("auth form, OK")
-    except Exception as e:
-        print(f"auth form, NotOK, {e}")
-    login_mail = browser.find_element_by_xpath(
-        "//input[@type='text']")  # проверка почты без собаки и незаполненный пароль
-    login_mail.send_keys("noatmail")
-    login_button = browser.find_element_by_xpath("//button[@class='mainBtn']")
-    login_button.click()
-    sleep(1)  # слип нужен чтоб форма обновилась
-    if login_mail.get_attribute("class") == "inputError":
-        print("mail !@, OK")
-    else:
-        print("mail !@, NotOK")
-    if browser.find_element_by_xpath("//input[@type='password']").get_attribute("class") == "inputError":
-        print("no password, OK")
-    else:
-        print("no password, NotOK")
-    if browser.page_source.find("Enter valid email address") > 0:
-        print("mail error messaage, OK")
-    else:
-        print("mail error messaage, OK")
-    if browser.page_source.find("This field is required") > 0:
-        browser.save_screenshot(f"{config.SCREENSHOT_PATH_AUTHORISATION}NoEtMailNoPasswordNuxbet.png")
-        print("empty field message, OK")
-    else:
-        print("empty field message, OK")
+    def input_login(self):
+        self.driver.interaction_with(Locators.INPUT_FIELD, text=config.AUTHORISATION_NAME)
 
-    login_mail = browser.find_element_by_xpath(
-        "//input[@type='text']")  # проверка валидной почты с незаполненным паролем
-    login_mail.send_keys(config.AUTHORISATION_NAME_EXIST)
-    login_button = browser.find_element_by_css_selector(".btnWrap > .mainBtn")
-    login_button.click()
-    sleep(1)  # слип нужен чтоб форма обновилась
-    if login_mail.get_attribute("class") != "inputError":
-        browser.save_screenshot(f"{config.SCREENSHOT_PATH_AUTHORISATION}NoPasswordNuxbet.png")
-        print("valid mail, OK")
-    else:
-        print("valid mail, NotOK")
-    browser.refresh()
+    def input_password(self):
+        self.driver.interaction_with(Locators.PASSWORD_FIELD.format('password'), text=config.PASSWORD)
 
-    login_mail = browser.find_element_by_xpath("//input[@type='text']")  # проверка валидной почты с неверным паролем
-    login_mail.send_keys(config.AUTHORISATION_NAME_EXIST)
-    login_button = browser.find_element_by_xpath("//form/div[2]/button")
-    password = browser.find_element_by_xpath("//input[@type='password']")
-    password.send_keys("password")
-    login_button.click()
-    sleep(1)  # слип нужен чтоб форма обновилась
-    if browser.page_source.find("Incorrect login or password. Please check again."):
-        browser.save_screenshot(f"{config.SCREENSHOT_PATH_AUTHORISATION}WrongPasswordNuxbet.png")
-        print("invalid password message, OK")
-    else:
-        print("invalid password message, NotOK")
-    browser.refresh()
+    def view_password(self):
+        self.driver.interaction_with(Locators.VIEW_PASSWORD_BUTTON, clickable=True, click=True)
+        return self.driver.interaction_with(Locators.INPUT_FIELD)[1].get_attribute("value")
 
+    def log_in(self):
+        self.driver.interaction_with(Locators.LOGIN_BUTTON, clickable=True, click=True)
 
-login_positive_flow()
-browser.refresh()
-login_negative_flow()
-browser.close()
+    def get_username(self):
+        return self.driver.interaction_with(Locators.USERNAME).text
+
+    def log_out(self):
+        self.driver.interaction_with(Locators.DROP_DOWN, clickable=True, click=True)
+        self.driver.interaction_with(Locators.LOGOUT, clickable=True, click=True)
+        return self.driver.interaction_with(Locators.GO_TO_LOGIN).text
+
--- tests.py
+++ tests.py
@@ -17,9 +17,16 @@ def test_login_prod(browser):
     test_page = LoginTest(browser)
     test_page.open_site()
     assert 'Log In' == test_page.open_login_form()
+    error_messages = test_page.valid_login()
+    assert 'Enter valid email address' == error_messages[0].text
+    assert 'This field is required' == error_messages[1].text
+    assert 'Incorrect login or password. Please check again.' == test_page.incorrect_login().strip()
+    assert 'Incorrect login or password. Please check again.' == test_page.incorrect_password().strip()
     test_page.input_login()
     test_page.input_password()
     assert config.PASSWORD == test_page.view_password()
-    test_page.login()
+    test_page.log_in()
     assert config.AUTHORISATION_NAME == test_page.get_username()
+    assert 'Log In' == test_page.log_out()
+
 
